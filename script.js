1b4e533e6eb65bfbcd0492b17a86a7efd605936f
Chumakov Mikhail <mcfrontend@yandex.ru>
2022-03-23 14:59:43 +0300

    ver_007

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 // Находим форму в DOM
 let popup = document.querySelector(".popup");
 let popupContainer = document.querySelector(".popup__container");// Воспользуйтесь методом querySelector()
+
 // Находим поля формы в DOM
+
 let popupName = document.querySelector(".popup__name");// Воспользуйтесь инструментом .querySelector()
 let popupSpeciality = document.querySelector(".popup__speciality");// Воспользуйтесь инструментом .querySelector()
-
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
-
 const popupCloseButton = popup.querySelector(".popup__close-button");
-
 const profileEditButton = document.querySelector(".profile__edit-button");
-
 const popupSave = document.querySelector(".popup__save");
-
 let cards = document.querySelector(".cards");
-
-/* const cardsHeardsButton = cards.querySelectorAll(".cards__heards_button");
-
-const cardsHeards = cards.querySelectorAll(".cards__heards"); */
 
 popupName.value= profileTitle.textContent;
 popupSpeciality.value = profileSubtitle.textContent;
-
 
 const buttonAdd = document.querySelector(".profile__add-button");
 const popupAdd = document.querySelector(".popup-add");
@@ -32,9 +24,6 @@ let popupAddContainer = document.querySelector(".popup-add__container");
 let overlayAdd = document.querySelector(".overlay-add");
 const popupCloseButtonAdd = document.querySelector(".popup-add__close-button");
 let popupContainerAdd = document.querySelector(".popup-add__container");
-/* const cardsTrashButton = document.querySelectorAll(".cards__trash_button");
-const cardsItem = document.querySelectorAll(".cards__item"); */
-
 
 const initialCards = [
   {
@@ -63,11 +52,13 @@ const initialCards = [
   }
 ];
 
+// popup add cards
+
 for (let i = 0; i < initialCards.length; i++) {
   //console.log(initialCards[i]);
   cards.innerHTML += `
   <li class="cards__item">
-    <img class="cards__img" src="${initialCards[i].link}" alt="${initialCards[i].link}">
+  <input class="cards__img" type="image" src="${initialCards[i].link}" alt="${initialCards[i].link}">
     <button class="cards__trash_button"><img class="cards__trash" src="./images/Trash.svg"></button>
     <div class="cards__info">
       <h3 class="cards__title">${initialCards[i].name}</h3>
@@ -75,17 +66,6 @@ for (let i = 0; i < initialCards.length; i++) {
     </div>
   </li>
   `;
-
-  const cardsTrashButton = document.querySelectorAll(".cards__trash_button");
-  const cardsItem = document.querySelectorAll(".cards__item");
-
-  for (let j = 0; cardsItem > j; j++) {
-    for (let i = 0; cardsTrashButton.length > i; i++) {
-      cardsTrashButton.addEventListener("click", function() {
-        cardsItem[j].remove();
-      });
-    }
-  }
 }
 
 function formSubmitHandler2 (evt) {
@@ -94,7 +74,7 @@ function formSubmitHandler2 (evt) {
   popupСreate.addEventListener("click", function(evt) { 
     cards.innerHTML =  `
     <li class="cards__item">
-      <img class="cards__img" src="${document.querySelector(".popup-add__link").value}" alt="${document.querySelector(".popup-add__link").value}">
+    <input class="cards__img" type="image" src="${document.querySelector(".popup-add__link").value}" alt="${document.querySelector(".popup-add__link").value}">
       <button class="cards__trash_button"><img class="cards__trash" src="./images/Trash.svg"></button>
       <div class="cards__info">
         <h3 class="cards__title">${document.querySelector(".popup-add__name").value}</h3>
@@ -103,6 +83,7 @@ function formSubmitHandler2 (evt) {
     </li>
     ` + cards.innerHTML;
 
+    // написать условие, что if cardsItem >= 6 - удалить последнюю карточку
     cards.removeChild(cards.lastChild);
 
     const cardsHeardsButton = cards.querySelectorAll(".cards__heards_button");
@@ -113,6 +94,7 @@ function formSubmitHandler2 (evt) {
         cardsHeards[i].classList.toggle("cards__heards-black");
       });
     }
+
   });
 
   popupСreate.addEventListener("click", function() {
@@ -120,9 +102,44 @@ function formSubmitHandler2 (evt) {
     popupAdd.classList.add("popup-add");
     /* overlayAdd.classList.remove("overlay-add"); */
   });
+
+  const cardsTrashButton = document.querySelectorAll(".cards__trash_button");
+  const cardsItem = document.querySelectorAll(".cards__item");
+  
+  for (let j = 0; cardsItem.length > j; j++) {
+    for (let i = 0; cardsTrashButton.length > i; i++) {
+      cardsTrashButton[i].addEventListener("click", function() {
+        cardsItem[i].remove();
+      });
+    }
+  }
+
+  const popupImg = document.querySelector(".popup-img");
+  const cardsImg = document.querySelectorAll(".cards__img");
+  const cardsTitle = document.querySelectorAll(".cards__title"); // написать цикл для названий, что бы обращаться к popup-img__title
+
+  for (let i = 0; cardsImg.length > i; i++){ 
+    cardsImg[i].addEventListener("click", function() {
+      popupImg.classList.add("popup-img_opened");
+      popupImg.classList.remove("popup-img");
+
+      popupImg.innerHTML = `
+        <button class="popup-img__close-button hover-active" type="button"><img class="popup__close-icon" src="images/Close_Icon.svg"alt="закрыть">
+        </button>
+        <img class="popup-img__full-size" src="${cardsImg[i].src}">
+        <p class="popup-img__title">${document.querySelector(".popup-add__name").value}</p>
+        `;
+    });
+  }
 }
 
-
+document.addEventListener("keydown", function(event){
+  if (event.key === "Escape") {
+    popupAdd.classList.remove("popup-add_opened");
+    popupAdd.classList.add("popup-add");
+      /* overlay.classList.remove("overlay"); */
+  }
+});
 
 popupContainerAdd.addEventListener("click", formSubmitHandler2);
 
@@ -148,34 +165,37 @@ for (let i = 0; cardsHeardsButton.length > i; i++) {
   });
 }
 
+// popup name & speciality
+
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
+
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  
+                                              // Так мы можем определить свою логику отправки.
+                                              // О том, как это делать, расскажем позже.
 
 
-    popupName.textContent = profileTitle.value; 
-    popupSpeciality.textContent = profileSubtitle.value;// Получите значение полей jobInput и nameInput из свойства value
+  popupName.textContent = profileTitle.value; 
+  popupSpeciality.textContent = profileSubtitle.value; // Получите значение полей jobInput и nameInput из свойства value
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-    popupSave.addEventListener("click", function() {
+  // Выберите элементы, куда должны быть вставлены значения полей
+  popupSave.addEventListener("click", function() {
+    profileTitle.textContent = popupName.value;
+    profileSubtitle.textContent = popupSpeciality.value;
+    popup.classList.remove("popup_opened");
+    overlay.classList.remove("overlay");
+  });
+
+  document.addEventListener("keydown", function(event){
+    if (event.key === "Enter") {
       profileTitle.textContent = popupName.value;
       profileSubtitle.textContent = popupSpeciality.value;
       popup.classList.remove("popup_opened");
       overlay.classList.remove("overlay");
-    });
-
-    document.addEventListener("keydown", function(event){
-        if (event.key === "Enter") {
-          profileTitle.textContent = popupName.value;
-          profileSubtitle.textContent = popupSpeciality.value;
-          popup.classList.remove("popup_opened");
-          overlay.classList.remove("overlay");
-        }
-    });
+    }
+  });
     // Вставьте новые значения с помощью textContent
 }
 
@@ -192,7 +212,7 @@ profileEditButton.addEventListener("click", function() {
 popupCloseButton.addEventListener("click", function() {
     popup.classList.remove("popup_opened");
     /* overlay.classList.remove("overlay"); */
-    popupName.value= profileTitle.textContent;
+    popupName.value = profileTitle.textContent;
     popupSpeciality.value = profileSubtitle.textContent;
 });/* прячем окно при нажатии на крест, убираем класс popup_opened */
 
@@ -203,9 +223,46 @@ document.addEventListener("keydown", function(event){
     }
 });
 
-/*
-for (let i = 0; cardsTrashButton.length > i; i++) {
-  cardsTrashButton.addEventListener("click", function() {
-    cardsTrashButton[i].parentElement.parentElement.remove();
+// popup full size img
+// (так же создано в popup add cards)
+
+const popupImg = document.querySelector(".popup-img");
+const cardsImg = document.querySelectorAll(".cards__img");
+const cardsTitle = document.querySelectorAll(".cards__title"); // написать цикл для названий, что бы обращаться к popup-img__title
+
+for (let i = 0; cardsImg.length > i; i++){ 
+  cardsImg[i].addEventListener("click", function() {
+    popupImg.classList.add("popup-img_opened");
+    popupImg.classList.remove("popup-img");
+
+    popupImg.innerHTML = `
+      <button class="popup-img__close-button hover-active" type="button"><img class="popup__close-icon" src="images/Close_Icon.svg"alt="закрыть">
+      </button>
+      <img class="popup-img__full-size" src="${cardsImg[i].src}">
+      <p class="popup-img__title">${document.querySelector(".popup-add__name").value}</p>
+      `;
   });
-} */
+  
+  document.addEventListener("keydown", function(event){
+    if (event.key === "Escape") {
+      popupImg.classList.remove("popup-img_opened");
+      popupImg.classList.add("popup-img");
+        /* overlay.classList.remove("overlay"); */
+    }
+  });
+}
+
+// удаление карточек
+// (так же создано в popup add cards)
+
+const cardsTrashButton = document.querySelectorAll(".cards__trash_button");
+
+const cardsItem = document.querySelectorAll(".cards__item");
+
+for (let j = 0; cardsItem.length > j; j++) {
+  for (let i = 0; cardsTrashButton.length > i; i++) {
+    cardsTrashButton[i].addEventListener("click", function() {
+      cardsItem[i].remove();
+    });
+  }
+}
